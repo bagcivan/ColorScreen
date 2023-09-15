1f80b4bbfd163d74447883c2facd563529f17c61
TEVFIK BAGCIVAN <tevfik.bagcivan@gmail.com>
2023-09-16 00:23:49 +0300

    "optimize code"

--- script.js
+++ script.js
@@ -1,73 +1,65 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    let wakeLock = null;
+    const FFT_SIZE = 1024;
+    const MULTIPLIER = 2;
 
-    const requestWakeLock = async () => {
+    // Ekranın aktif kalmasını sağlama
+    async function activateWakeLock() {
+        let wakeLock = null;
+
         try {
             if ('WakeLock' in window && 'request' in window.WakeLock) {
                 const controller = new AbortController();
-                const signal = controller.signal;
-                await window.WakeLock.request('screen', { signal });
-                console.log('Wake Lock is active');
+                await window.WakeLock.request('screen', { signal: controller.signal });
             } else if ('wakeLock' in navigator && 'request' in navigator.wakeLock) {
                 wakeLock = await navigator.wakeLock.request('screen');
-                wakeLock.addEventListener('release', (e) => {
+                wakeLock.addEventListener('release', () => {
                     console.log('Wake Lock was released');
                 });
-                console.log('Wake Lock is active');
             } else {
-                console.error('Wake Lock API not supported.');
+                throw new Error('Wake Lock API not supported.');
             }
+            console.log('Wake Lock is active');
         } catch (e) {
             console.error(`${e.name}, ${e.message}`);
         }
-    };
+    }
 
-    // Siteye girer girmez bu fonksiyon çalışsın
-    requestWakeLock();
+    // Ses analizi ve arkaplan rengini ayarlama
+    function analyzeAudio() {
+        navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+            .then(stream => {
+                const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+                const analyser = audioContext.createAnalyser();
+                analyser.fftSize = FFT_SIZE;
+                const bufferLength = analyser.frequencyBinCount;
+                const dataArray = new Uint8Array(bufferLength);
+                const source = audioContext.createMediaStreamSource(stream);
+                source.connect(analyser);
 
+                function getAverageVolume(array) {
+                    return array.reduce((acc, value) => acc + value, 0) / array.length;
+                }
 
+                function updateVisualizer() {
+                    requestAnimationFrame(updateVisualizer);
 
-    navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-        .then(stream => {
-            const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-            const analyser = audioContext.createAnalyser();
-            analyser.fftSize = 1024;  // FFT boyutunu artırma
-            const bufferLength = analyser.frequencyBinCount;
-            const dataArray = new Uint8Array(bufferLength);
+                    analyser.getByteFrequencyData(dataArray);
 
-            const source = audioContext.createMediaStreamSource(stream);
-            source.connect(analyser);
+                    const bass = getAverageVolume(dataArray.slice(0, bufferLength / 3)) * MULTIPLIER;
+                    const mid = getAverageVolume(dataArray.slice(bufferLength / 3, (bufferLength * 2) / 3)) * MULTIPLIER;
+                    const treble = getAverageVolume(dataArray.slice((bufferLength * 2) / 3, bufferLength)) * MULTIPLIER;
 
-            function getAverageVolume(array) {
-                const values = array.reduce((acc, value) => acc + value, 0);
-                return values / array.length;
-            }
+                    document.body.style.backgroundColor = `rgb(${Math.min(255, bass)}, ${Math.min(255, mid)}, ${Math.min(255, treble)})`;
+                }
 
-            function update() {
-                requestAnimationFrame(update);
+                updateVisualizer();
+            })
+            .catch(err => {
+                console.error('Mikrofon erişimi reddedildi:', err);
+            });
+    }
 
-                analyser.getByteFrequencyData(dataArray);
-
-                // Bas, orta ve tiz frekans aralıklarını tanımlama
-                const bassArray = dataArray.slice(0, bufferLength / 3);
-                const midArray = dataArray.slice(bufferLength / 3, (bufferLength * 2) / 3);
-                const trebleArray = dataArray.slice((bufferLength * 2) / 3, bufferLength);
-
-                const bass = getAverageVolume(bassArray) * 2;  // Hassasiyeti artırma
-                const mid = getAverageVolume(midArray) * 2;    // Hassasiyeti artırma
-                const treble = getAverageVolume(trebleArray) * 2;  // Hassasiyeti artırma
-
-                const red = Math.min(255, bass);
-                const green = Math.min(255, mid);
-                const blue = Math.min(255, treble);
-
-                document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-            }
-
-            update();
-        })
-        .catch(err => {
-            console.error('Mikrofon erişimi reddedildi:', err);
-        });
+    activateWakeLock();
+    analyzeAudio();
 });
